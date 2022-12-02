489cd880e70eb6eb518ae7d5d4b0e865b3fbbdaa
unknown <kaniek99@gmail.com>
2022-12-02 21:04:20 +0100

    solve day2 puzzle

--- day2/day2.go
+++ day2/day2.go
@@ -39,44 +39,51 @@ func main() {
 	myPoints := 0
 	elfPoints := 0
 	for _, line := range data {
-		// rock = 0 paper = 1 scisor = 2
-		battle := Battle{elf: int(line[0]) - 65, me: int(line[2]) - 88}
+		battle := Battle{elf: int(line[0]) - 64, me: int(line[2]) - 87}
 		switch battle.result() {
 		// score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 		// plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 		case Draw:
-			elfPoints += battle.elf + 1 + 3
-			myPoints += battle.me + 1 + 3
+			elfPoints += battle.elf + 3
+			myPoints += battle.me + 3
 		case Lose:
-			elfPoints += battle.elf + 1 + 6
-			myPoints += battle.me + 1
+			elfPoints += battle.elf + 6
+			myPoints += battle.me
 		case Win:
-			elfPoints += battle.elf + 1
-			myPoints += battle.me + 1 + 6
+			elfPoints += battle.elf
+			myPoints += battle.me + 6
 		}
 	}
 	fmt.Println("Points earned by me:", myPoints)
 	fmt.Println("Points earned by elf:", elfPoints)
 
-	//second part X means I need to lose, Y draw, Z win X <-> 0 Y <-> 1 Z <-> 2
+	//second part: X means I need to lose, Y draw, Z win X <-> 1 Y <-> 2 Z <-> 3
 	myPointsPart2 := 0
 	elfPointsPart2 := 0
 	for _, line := range data {
-		battle := Battle{elf: int(line[0]) - 65, me: int(line[2]) - 88}
+		battle := Battle{elf: int(line[0]) - 64, me: int(line[2]) - 87}
 		switch battle.me {
-		case 0:
-			elfPointsPart2 += battle.elf + 1 + 6
-			myPointsPart2 += (battle.elf+1)%3 + 1
 		case 1:
-			elfPointsPart2 += battle.elf + 1 + 3
-			myPointsPart2 += battle.elf + 1 + 3
+			if battle.elf == 1 {
+				elfPointsPart2 += battle.elf + 6
+				myPointsPart2 += 3
+			} else {
+				elfPointsPart2 += battle.elf + 6
+				myPointsPart2 += battle.elf - 1
+			}
 		case 2:
-			elfPointsPart2 += battle.elf + 1
-			myPointsPart2 += (battle.elf-1)%3 + 6
+			elfPointsPart2 += battle.elf + 3
+			myPointsPart2 += battle.elf + 3
+		case 3:
+			if battle.elf == 3 {
+				elfPointsPart2 += battle.elf
+				myPointsPart2 += 7
+			} else {
+				elfPointsPart2 += battle.elf
+				myPointsPart2 += battle.elf + 1 + 6
+			}
 		}
 	}
 	fmt.Println("Points earned by me:", myPointsPart2)
 	fmt.Println("Points earned by elf:", elfPointsPart2)
 }
-
-//wrong answers: 11225 12083 12266
